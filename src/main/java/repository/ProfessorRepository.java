eb5448f49ff8b789ff2c2f0aaad65ce803a73222
douglas <douglas.rtoledo2003@gmail.com>
2025-11-12 01:05:59 -0300

    feat: adicionar busca por CPF no ProfessorRepository

--- src/main/java/repository/ProfessorRepository.java
+++ src/main/java/repository/ProfessorRepository.java
@@ -8,7 +8,11 @@ public interface ProfessorRepository {
     boolean update(Professor professor);
     boolean delete(int id);
     Professor findById(int id);
+    
+    Professor findByCpf(String cpf);
+    
     List<Professor> findAll();
+    
     int getMaxId();
 }
 
