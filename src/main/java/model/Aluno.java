028bfd28855a59644be95ac862b75380f6662848
Luiz Felipe Correa Soares <1072223007@unisul.br>
2025-11-16 16:51:59 -0300

    criado javadoc

--- src/main/java/model/Aluno.java
+++ src/main/java/model/Aluno.java
@@ -5,22 +5,59 @@ import repository.AlunoRepository;
 import repository.AlunoRepositoryImpl;
 import java.sql.SQLException;
 
+/**
+ * Classe que representa um aluno no sistema.
+ * Estende a classe Pessoa e adiciona informações específicas de aluno como curso e fase.
+ * 
+ * @author Sistema GQS
+ * @version 1.0
+ */
 public class Aluno extends Pessoa {
 
+    /**
+     * Nome do curso em que o aluno está matriculado.
+     */
     private String curso;
+
+    /**
+     * Fase/periodo atual do aluno no curso.
+     */
     private int fase;
+
+    /**
+     * Repositório responsável pelas operações de persistência de alunos.
+     */
     private final AlunoRepository repository;
 
+    /**
+     * Construtor padrão sem parâmetros.
+     * Inicializa o repositório de alunos.
+     */
     public Aluno() {
         this.repository = new AlunoRepositoryImpl();
     }
 
+    /**
+     * Construtor que inicializa apenas os atributos específicos de aluno.
+     * 
+     * @param curso Nome do curso do aluno
+     * @param fase  Fase/periodo atual do aluno
+     */
     public Aluno(String curso, int fase) {
         this.curso = curso;
         this.fase = fase;
         this.repository = new AlunoRepositoryImpl();
     }
 
+    /**
+     * Construtor completo que inicializa todos os atributos do aluno.
+     * 
+     * @param curso Nome do curso do aluno
+     * @param fase  Fase/periodo atual do aluno
+     * @param id    Identificador único do aluno
+     * @param nome  Nome completo do aluno
+     * @param idade Idade do aluno em anos
+     */
     public Aluno(String curso, int fase, int id, String nome, int idade) {
         super(id, nome, idade);
         this.curso = curso;
@@ -28,22 +65,47 @@ public class Aluno extends Pessoa {
         this.repository = new AlunoRepositoryImpl();
     }
 
+    /**
+     * Retorna o nome do curso do aluno.
+     * 
+     * @return O nome do curso
+     */
     public String getCurso() {
         return curso;
     }
 
+    /**
+     * Define o nome do curso do aluno.
+     * 
+     * @param curso O nome do curso a ser atribuído
+     */
     public void setCurso(String curso) {
         this.curso = curso;
     }
 
+    /**
+     * Retorna a fase/periodo atual do aluno.
+     * 
+     * @return A fase do aluno
+     */
     public int getFase() {
         return fase;
     }
 
+    /**
+     * Define a fase/periodo atual do aluno.
+     * 
+     * @param fase A fase a ser atribuída ao aluno
+     */
     public void setFase(int fase) {
         this.fase = fase;
     }
 
+    /**
+     * Retorna uma representação em string do aluno.
+     * 
+     * @return String formatada com todas as informações do aluno
+     */
     @Override
     public String toString() {
         return "\n ID: " + this.getId()
@@ -54,28 +116,71 @@ public class Aluno extends Pessoa {
                 + "\n -----------";
     }
 
+    /**
+     * Retorna uma lista com todos os alunos cadastrados no banco de dados.
+     * 
+     * @return ArrayList contendo todos os alunos cadastrados
+     */
     public ArrayList getMinhaLista() {
         return new ArrayList(repository.findAll());
     }
 
+    /**
+     * Insere um novo aluno no banco de dados.
+     * 
+     * @param curso Nome do curso do aluno
+     * @param fase  Fase/periodo atual do aluno
+     * @param nome  Nome completo do aluno
+     * @param idade Idade do aluno em anos
+     * @return true se o aluno foi inserido com sucesso, false caso contrário
+     * @throws SQLException Se ocorrer um erro ao acessar o banco de dados
+     */
     public boolean InsertAlunoBD(String curso, int fase, String nome, int idade) throws SQLException {
         Aluno objeto = new Aluno(curso, fase, 0, nome, idade);
         return repository.save(objeto);
     }
 
+    /**
+     * Remove um aluno do banco de dados pelo ID.
+     * 
+     * @param id Identificador único do aluno a ser removido
+     * @return true se o aluno foi removido com sucesso, false caso contrário
+     */
     public boolean DeleteAlunoBD(int id) {
         return repository.delete(id);
     }
 
+    /**
+     * Atualiza os dados de um aluno existente no banco de dados.
+     * 
+     * @param curso Nome do curso do aluno
+     * @param fase  Fase/periodo atual do aluno
+     * @param id    Identificador único do aluno a ser atualizado
+     * @param nome  Nome completo do aluno
+     * @param idade Idade do aluno em anos
+     * @return true se o aluno foi atualizado com sucesso, false caso contrário
+     */
     public boolean UpdateAlunoBD(String curso, int fase, int id, String nome, int idade) {
         Aluno objeto = new Aluno(curso, fase, id, nome, idade);
         return repository.update(objeto);
     }
 
+    /**
+     * Carrega um aluno do banco de dados pelo ID.
+     * 
+     * @param id Identificador único do aluno a ser carregado
+     * @return O objeto Aluno encontrado, ou null se não encontrado
+     */
     public Aluno carregaAluno(int id) {
         return repository.findById(id);
     }
 
+    /**
+     * Retorna o maior ID cadastrado no banco de dados.
+     * 
+     * @return O maior ID encontrado, ou 0 se não houver registros
+     * @throws SQLException Se ocorrer um erro ao acessar o banco de dados
+     */
     public int maiorID() throws SQLException {
         return repository.getMaxId();
     }
--- src/main/java/model/Pessoa.java
+++ src/main/java/model/Pessoa.java
@@ -1,40 +1,99 @@
 package model;
 
+/**
+ * Classe abstrata que representa uma pessoa no sistema.
+ * Serve como classe base para Aluno e Professor, contendo atributos comuns.
+ * 
+ * @author Sistema GQS
+ * @version 1.0
+ */
 public abstract class Pessoa {
 
+    /**
+     * Identificador único da pessoa no sistema.
+     */
     private int id;
+
+    /**
+     * Nome completo da pessoa.
+     */
     private String nome;
+
+    /**
+     * Idade da pessoa em anos.
+     */
     private int idade;
 
+    /**
+     * Construtor padrão sem parâmetros.
+     * Inicializa uma instância de Pessoa com valores padrão.
+     */
     public Pessoa() {
     }
 
+    /**
+     * Construtor completo que inicializa todos os atributos da pessoa.
+     * 
+     * @param id    Identificador único da pessoa
+     * @param nome  Nome completo da pessoa
+     * @param idade Idade da pessoa em anos
+     */
     public Pessoa(int id, String nome, int idade) {
         this.id = id;
         this.nome = nome;
         this.idade = idade;
     }
 
+    /**
+     * Retorna o identificador único da pessoa.
+     * 
+     * @return O ID da pessoa
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Define o identificador único da pessoa.
+     * 
+     * @param id O ID a ser atribuído à pessoa
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Retorna o nome completo da pessoa.
+     * 
+     * @return O nome da pessoa
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Define o nome completo da pessoa.
+     * 
+     * @param nome O nome a ser atribuído à pessoa
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Retorna a idade da pessoa em anos.
+     * 
+     * @return A idade da pessoa
+     */
     public int getIdade() {
         return idade;
     }
 
+    /**
+     * Define a idade da pessoa em anos.
+     * 
+     * @param idade A idade a ser atribuída à pessoa
+     */
     public void setIdade(int idade) {
         this.idade = idade;
     }
